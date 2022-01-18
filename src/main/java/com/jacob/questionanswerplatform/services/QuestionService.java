faf0f41e2c382033e7da74bb30fc7706a177ba4d
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-01-18 12:06:19 +0530

    Removed Optional type as function parameters.

--- src/main/java/com/jacob/questionanswerplatform/services/QuestionService.java
+++ src/main/java/com/jacob/questionanswerplatform/services/QuestionService.java
@@ -19,107 +19,98 @@ import java.util.stream.Collectors;
 @Service
 public class QuestionService {
 
-	private final QuestionDAO   questionDAO;
-	private final CompanyDAO    companyDAO;
-	private final SubTopicDAO   subTopicDAO;
-	private final UserDAO       userDAO;
-	private final AnswerService answerService;
+    private final QuestionDAO questionDAO;
+    private final CompanyDAO companyDAO;
+    private final SubTopicDAO subTopicDAO;
+    private final UserDAO userDAO;
+    private final AnswerService answerService;
 
-	public QuestionService(QuestionDAO questionDAO, CompanyDAO companyDAO, SubTopicDAO subTopicDAO, UserDAO userDAO,
-	                       AnswerService answerService) {
-		this.questionDAO = questionDAO;
-		this.companyDAO = companyDAO;
-		this.subTopicDAO = subTopicDAO;
-		this.userDAO = userDAO;
-		this.answerService = answerService;
-	}
+    public QuestionService(
+            QuestionDAO questionDAO,
+            CompanyDAO companyDAO,
+            SubTopicDAO subTopicDAO,
+            UserDAO userDAO,
+            AnswerService answerService) {
+        this.questionDAO = questionDAO;
+        this.companyDAO = companyDAO;
+        this.subTopicDAO = subTopicDAO;
+        this.userDAO = userDAO;
+        this.answerService = answerService;
+    }
 
-	public Long postQuestion(PostQuestionDTO postQuestionDTO) {
-		Question question = new Question();
-		question.setQuestionText(postQuestionDTO.getQuestionText());
-		question.setAnswers(new ArrayList<>());
+    public Long postQuestion(PostQuestionDTO postQuestionDTO) {
+        Question question = new Question();
+        question.setQuestionText(postQuestionDTO.getQuestionText());
+        question.setAnswers(new ArrayList<>());
 
-		Long companyId = postQuestionDTO.getCompanyId();
-		if (companyId != null) {
-			companyDAO.findById(companyId)
-			          .ifPresent(question.getCompanies()::add);
-		}
+        Long companyId = postQuestionDTO.getCompanyId();
+        if (companyId != null) {
+            companyDAO.findById(companyId).ifPresent(question.getCompanies()::add);
+        }
 
-		userDAO.findById(postQuestionDTO.getUserId())
-		       .ifPresent(question::setUser);
+        userDAO.findById(postQuestionDTO.getUserId()).ifPresent(question::setUser);
 
-		subTopicDAO.findById(postQuestionDTO.getSubTopicId())
-		           .ifPresent(question.getSubTopics()::add);
+        subTopicDAO
+                .findById(postQuestionDTO.getSubTopicId())
+                .ifPresent(question.getSubTopics()::add);
 
-		List<Tag> tags = postQuestionDTO.getTags();
-		if (tags != null) {
-			question.getTags().addAll(tags);
-		}
+        List<Tag> tags = postQuestionDTO.getTags();
+        if (tags != null) {
+            question.getTags().addAll(tags);
+        }
 
-		return questionDAO.saveAndFlush(question).getId();
-	}
+        return questionDAO.saveAndFlush(question).getId();
+    }
 
-	public GetQuestionDTO getQuestionDTO(Long id) {
-		Optional<Question> optionalQuestion = questionDAO.findById(id);
-		if (optionalQuestion.isEmpty()) throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+    public GetQuestionDTO getQuestionDTO(Long id) {
+        Optional<Question> optionalQuestion = questionDAO.findById(id);
+        if (optionalQuestion.isEmpty()) throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
 
-		Question question = optionalQuestion.get();
+        Question question = optionalQuestion.get();
 
-		return getQuestionDTO(question);
-	}
+        return getQuestionDTO(question);
+    }
 
-	public GetQuestionDTO getQuestionDTO(Question question) {
-		GetQuestionDTO questionDTO = new GetQuestionDTO();
+    public GetQuestionDTO getQuestionDTO(Question question) {
+        GetQuestionDTO questionDTO = new GetQuestionDTO();
 
-		questionDTO.setQuestionText(question.getQuestionText());
-		questionDTO.setCompanies(question.getCompanies());
-		questionDTO.setSubTopics(question.getSubTopics());
-		questionDTO.setTags(question.getTags());
-		questionDTO.setLikes(question.getLikes());
+        questionDTO.setQuestionText(question.getQuestionText());
+        questionDTO.setCompanies(question.getCompanies());
+        questionDTO.setSubTopics(question.getSubTopics());
+        questionDTO.setTags(question.getTags());
+        questionDTO.setLikes(question.getLikes());
 
-		List<GetAnswerDTO> answerDTOs = new ArrayList<>();
+        List<GetAnswerDTO> answerDTOs = new ArrayList<>();
 
-		question.getAnswers()
-		        .forEach(answer -> answerDTOs.add(answerService.getAnswerDTO(answer)));
+        question.getAnswers().forEach(answer -> answerDTOs.add(answerService.getAnswerDTO(answer)));
 
-		questionDTO.setAnswers(answerDTOs);
+        questionDTO.setAnswers(answerDTOs);
 
-		return questionDTO;
+        return questionDTO;
+    }
 
-	}
+    public void like(QuestionLikeDTO questionLikeDTO) {
+        if (userDAO.findById(questionLikeDTO.getUserId()).isEmpty())
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
 
-	public void like(QuestionLikeDTO questionLikeDTO) {
-		if (userDAO.findById(questionLikeDTO.getUserId()).isEmpty())
-			throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        Optional<Question> optionalQuestion = questionDAO.findById(questionLikeDTO.getQuestionId());
 
-		Optional<Question> optionalQuestion = questionDAO.findById(questionLikeDTO.getQuestionId());
+        if (optionalQuestion.isEmpty()) throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
 
-		if (optionalQuestion.isEmpty()) throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        Question question = optionalQuestion.get();
+        question.setLikes(question.getLikes() + 1);
 
-		Question question = optionalQuestion.get();
-		question.setLikes(question.getLikes() + 1);
+        questionDAO.saveAndFlush(question);
+    }
 
-		questionDAO.saveAndFlush(question);
-	}
-
-	public List<GetQuestionFilterDTO> filterBy(PostQuestionFilterDTO questionFilterDTO) {
-		return Question.stream(questionDAO.findAll())
-		               .filterByLikesGreaterThan(Optional.ofNullable(questionFilterDTO.getLikesCount()))
-		               .filterByDatePostedOn(Optional.ofNullable(questionFilterDTO.getDate()))
-		               .filterByContains(
-				               Optional.ofNullable(questionFilterDTO.getCompanies()),
-				               Question::getCompanies
-		               )
-		               .filterByContains(
-				               Optional.ofNullable(questionFilterDTO.getTags()),
-				               Question::getTags
-		               )
-		               .filterByContains(
-				               Optional.ofNullable(questionFilterDTO.getSubTopics()),
-				               Question::getSubTopics
-		               )
-		               .toFilterDTO()
-		               .collect(Collectors.toList());
-	}
-
+    public List<GetQuestionFilterDTO> filterBy(PostQuestionFilterDTO questionFilterDTO) {
+        return Question.stream(questionDAO.findAll())
+                .filterByLikesGreaterThan(questionFilterDTO.getLikesCount())
+                .filterByDatePostedOn(questionFilterDTO.getDate())
+                .filterByContains(questionFilterDTO.getCompanies(), Question::getCompanies)
+                .filterByContains(questionFilterDTO.getTags(), Question::getTags)
+                .filterByContains(questionFilterDTO.getSubTopics(), Question::getSubTopics)
+                .toFilterDTO()
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/jacob/questionanswerplatform/utils/streams/QuestionStream.java
+++ src/main/java/com/jacob/questionanswerplatform/utils/streams/QuestionStream.java
@@ -2,85 +2,85 @@ package com.jacob.questionanswerplatform.utils.streams;
 
 import com.jacob.questionanswerplatform.dtos.GetQuestionFilterDTO;
 import com.jacob.questionanswerplatform.models.*;
+import org.springframework.lang.Nullable;
 
 import java.sql.Date;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class QuestionStream implements ForwardingStream<Question> {
 
-	private final List<Question> questions;
+    private final List<Question> questions;
 
-	public QuestionStream(List<Question> questions) {
-		this.questions = questions;
-	}
+    public QuestionStream(List<Question> questions) {
+        this.questions = questions;
+    }
 
-	public QuestionStream filterByLikesGreaterThan(Optional<Integer> optionalLikesCount) {
-		if (optionalLikesCount.isEmpty()) return Question.stream(this.questions);
-		int likesCount = optionalLikesCount.get();
+    public QuestionStream filterByLikesGreaterThan(@Nullable Integer likesCount) {
+        if (likesCount == null) return Question.stream(this.questions);
 
-		return Question.stream(this.getStream()
-		                           .filter(question -> question.getLikes() > likesCount)
-		                           .collect(Collectors.toList())
-		);
-	}
+        return Question.stream(
+                this.getStream()
+                        .filter(question -> question.getLikes() > likesCount)
+                        .collect(Collectors.toList()));
+    }
 
-	@Override
-	public Stream<Question> getStream() {
-		return this.questions.stream();
-	}
+    @Override
+    public Stream<Question> getStream() {
+        return this.questions.stream();
+    }
 
-	public QuestionStream filterByDatePostedOn(Optional<Date> optionalDate) {
-		if (optionalDate.isEmpty()) return Question.stream(this.questions);
-		Date date = optionalDate.get();
+    public QuestionStream filterByDatePostedOn(@Nullable Date date) {
+        if (date == null) return Question.stream(this.questions);
 
-		return Question.stream(this.getStream()
-		                           .filter(question -> question.getDate().toLocalDate().isEqual(date.toLocalDate()))
-		                           .collect(Collectors.toList())
-		);
-	}
+        return Question.stream(
+                this.getStream()
+                        .filter(
+                                question ->
+                                        question.getDate()
+                                                .toLocalDate()
+                                                .isEqual(date.toLocalDate()))
+                        .collect(Collectors.toList()));
+    }
 
-	public Stream<GetQuestionFilterDTO> toFilterDTO() {
-		return this.getStream()
-		           .map(question -> {
-			           GetQuestionFilterDTO getQuestionFilterDTO = new GetQuestionFilterDTO();
+    public Stream<GetQuestionFilterDTO> toFilterDTO() {
+        return this.getStream()
+                .map(
+                        question -> {
+                            GetQuestionFilterDTO getQuestionFilterDTO = new GetQuestionFilterDTO();
 
-			           getQuestionFilterDTO.setQuestionText(question.getQuestionText());
-			           getQuestionFilterDTO.setCompanies(question.getCompanies());
-			           getQuestionFilterDTO.setDate(question.getDate());
-			           getQuestionFilterDTO.setTags(question.getTags());
-			           getQuestionFilterDTO.setQuestionId(question.getId());
-			           getQuestionFilterDTO.setLikes(question.getLikes());
+                            getQuestionFilterDTO.setQuestionText(question.getQuestionText());
+                            getQuestionFilterDTO.setCompanies(question.getCompanies());
+                            getQuestionFilterDTO.setDate(question.getDate());
+                            getQuestionFilterDTO.setTags(question.getTags());
+                            getQuestionFilterDTO.setQuestionId(question.getId());
+                            getQuestionFilterDTO.setLikes(question.getLikes());
 
-			           question.getAnswers()
-			                   .stream()
-			                   .max(Comparator.comparingInt(Answer::getLikes))
-			                   .map(Answer::getAnswerText)
-			                   .ifPresentOrElse(
-					                   getQuestionFilterDTO::setMostLikedAnswer,
-					                   () -> getQuestionFilterDTO.setMostLikedAnswer("")
-			                   );
+                            question.getAnswers().stream()
+                                    .max(Comparator.comparingInt(Answer::getLikes))
+                                    .map(Answer::getAnswerText)
+                                    .ifPresentOrElse(
+                                            getQuestionFilterDTO::setMostLikedAnswer,
+                                            () -> getQuestionFilterDTO.setMostLikedAnswer(""));
 
-			           return getQuestionFilterDTO;
-		           });
-	}
+                            return getQuestionFilterDTO;
+                        });
+    }
 
-	public <T> QuestionStream filterByContains(Optional<List<T>> optionalFilterList,
-	                                           Function<Question, List<T>> getSourceList) {
-		if (optionalFilterList.isEmpty()) return Question.stream(this.questions);
-		List<T> companies = optionalFilterList.get();
+    public <T> QuestionStream filterByContains(
+            @Nullable List<T> filterList, Function<Question, List<T>> getSourceList) {
+        if (filterList == null) return Question.stream(this.questions);
 
-		return Question.stream(this.getStream()
-		                           .filter(question -> !Collections.disjoint(
-				                           companies,
-				                           getSourceList.apply(question)
-		                           ))
-		                           .collect(Collectors.toList())
-		);
-	}
+        return Question.stream(
+                this.getStream()
+                        .filter(
+                                question ->
+                                        !Collections.disjoint(
+                                                filterList, getSourceList.apply(question)))
+                        .collect(Collectors.toList()));
+    }
 }
